75865812c3465bce5514a05c9d5b772596343c3d
hgonzalezdvp <128415287+Hgonzalez91@users.noreply.github.com>
2023-11-09 11:19:03 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,86 +1,108 @@
-function valorMayor (n1, n2){
-    if (n1>n2){
-        console.log(`El número ${n1} es mayor que el número ${n2}`);
-    } else if(n1 === n2){
-        console.log(`El número ${n1} es igual que el número ${n2}`);
+function esMayor (){
+
+    let valor1 = parseInt(prompt('Escribe un primer número'));
+    let valor2 = parseInt(prompt('Escribe un segundo número'));
+    
+    if (valor1 == valor2){
+        alert(`El número ${valor1} es igual que el número ${valor2}`);
+    } else if(valor1>valor2){
+        alert(`El número ${valor1} es mayor que el número ${valor2}`);
     }else{
-        console.log(`El número ${n2} es mayor que el número ${n1}`);
+        alert(`El número ${valor1} es menor que el número ${valor2}`);
     }
 }
 
 function opBasicas() {
+
+    let numero1 = parseInt(prompt('Escribe un primer número'));
+    let numero2 = parseInt(prompt('Escribe un segundo número'));
     
+    alert(`Operaciones básicas de los números obtenidos\n
+        ${numero1} + ${numero2} = ${numero1+numero2}\n
+        ${numero1} - ${numero2} = ${numero1-numero2}\n
+        ${numero1} * ${numero2} = ${numero1*numero2}\n
+        ${numero1} / ${numero2} = ${numero1/numero2}`);
 }
 
+function raizCuadrada() {
 
-//FUNCION ES MAYOR
+    let raiz = parseInt(prompt('Escribe un número para saber su raíz cuadrada'));
+    alert(`La raiz cuadrada de ${raiz} es = ${Math.sqrt(raiz)}`);
 
-    /*if (valor1>valor2){
-        console.log(`El número ${valor1} es mayor que el número ${valor2}`);
-    } else if(valor1 == valor2){
-        console.log(`El número ${valor1} es igual que el número ${valor2}`);
-    }else{
-        console.log(`El número ${valor1} es mayor que el número ${valor2}`);
-    }*/
+}
 
+function longitudPalabra() {
+    let palabra = prompt('Escribe un primer número');
+    console.log(palabra.length);
+}
 
-//SUMA RESTA MULT DIV
-
-    /*let valor1 = parseInt(prompt('Escribe un primer número'));
-    let valor2 = parseInt(prompt('Escribe un segundo número'));
-    
-    console.log(valor1+valor2);
-    console.log(valor1-valor2);
-    console.log(valor1*valor2);
-    console.log(valor1/valor2);*/
-
-
-//RAIZ CUADRADA
-
-    /*let valor1 = parseInt(prompt('Escribe un primer número'));
-    console.log(Math.sqrt(valor1));*/
-
-    //DEVUELVE LONGITUD DE PALABRA
-    /*let palabra = prompt('Escribe un primer número');
-    console.log(palabra.length);*/
-
-
-//REVERTIR PALABRA
-
-    /* let palabra = prompt('Escribe una frase');
-    let nuevaPalabra = palabra;
+function invertirFrase() {
+    let palabra = prompt('Escribe una frase');
+    let invertido = "";
     for (i = palabra.length - 1; i >= 0; i--) {
-        nuevaPalabra +=palabra[i];
+        invertido += palabra[i];
     }
-    return nuevaPalabra; */
+    alert(invertido);
+}
+
+function repetirFrase() {
+
+
+    let frase = prompt("Escribe una fase");
+    let cuenta = prompt("¿Cuántas veces quieres que se repita la frase?");
+    alert(`${frase.repeat(cuenta)}`);
+
+}
+
+function addToList() {
     
+    let artistas = ["The Weeknd", "Taylor Swift"];
+    alert(`Esta es la lista de artistas musicales: \n ${artistas}`);
+    let valorAgregado = artistas.unshift(prompt("Escribe un artista para agregar a la lista"));
+    alert(artistas);
 
-//REPETIR FRASE
+}
 
-    /*let frase =prompt("Escribe una fase")
-    console.log(frase.repeat(3) + "/");*/
-
-
-//AGREGAR VALORES AL PRINCIPIO DEL ARRAY
-    
-    /* let artistas = ["the Weeknd", "Taylor Swift"];
-    let valorAgregado = artistas.unshift(prompt("Escribe el artista para agregar a la lista"));
-    console.log(artistas); */
-
-
-//REVERTIR LOS VALORES DEL ARRAY
-    
-    /*let artistas = ["the Weeknd", "Taylor Swift", "Micro TDH", "Neutro Shorty"];
+function revertirValores() {
+    let artistas = ["The Weeknd", "Rawayana", "Micro TDH", "Sandy & Papo"];
+    alert(`Tu lista: \n ${artistas}`)
     let valorAgregado = artistas.reverse();
-    console.log(artistas);*/
+    alert(`Tu lista invertida: \n ${artistas}`);
+}
 
-
-//SUMAR TODOS LOS NUMEROS DE UN ARRAY
+function sumarValores() {
     
-    /*let numeros = [1,2,3,4,5,6,7,8,9];
-    suma = 0;
-    for(i = 0; i <= numeros.length; i++){
-        numero = numeros[i];
-        suma += numero;
+    let numeros = [1,2,3,4,5,6,7,8,9];
+    let suma = numeros.reduce((a, b) => a + b, 0);
+    
+    console.log(suma);
+}
+
+function iterador(){
+    let count = parseInt(prompt('¿Cúantos números desea ver?'));
+    for(i=1; i<=count; i++){
+    console.log(i);
     }
-    console.log(suma);*/
+}
+
+function iteradorMultiplicado(){
+    let result; 
+    let count = parseInt(prompt('¿Cúantos números desea ver?'));
+    for(let i=1; i<=count; i++){
+    console.log(i);
+    result = i;
+    }
+    alert(`La cuenta de tu iteración multiplicado por 2 es = ${result*2}`)
+}
+
+function indiceDelIterador(){
+    let lista = [];
+    let count = parseInt(prompt('¿Cúantos números desea ver?'));
+    for(i=1; i<=count; i++){
+    lista.push(i); 
+    }
+    lista.forEach((item, index, arr) => {
+        console.log(`Índice = [${index}] | Item = ${item}`)
+    });
+    
+}
